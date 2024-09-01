fdf3656adea7808690d76f95fa62e221efaee5e9
EnggelsRai <159988231+EnggelsRai@users.noreply.github.com>
2024-09-01 09:08:49 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import pickle
+
+import cv2
+import face_recognition
+
+# Select the webcam of the computer (0 by default for laptop)
+# Initialize the camera with a specific index
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)     # Width
+cap.set(4, 480)      # Height
+
+# Read the background image
+imgBackground = cv2.imread('Resources/background.png')
+
+# Importing the mode into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(len(imgModeList))
+
+# Load the encoding file
+print("Loading Encode file ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, employeeIds = encodeListKnownWithIds
+# print(employeeIds)
+print("Encode File Loaded")
+
+while True:
+
+    # Capture every frame
+
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("Matches", matches)
+        print("FaceDis", faceDis)
+
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
